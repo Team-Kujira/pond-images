55daedfe98c65eb1082158f730fa7eb38efa92e1
starsquidnodes <info@starsquid.io>
2024-02-10 13:14:10 +0100

    Rework prepare, add kujira-2, relayer

--- build.py
+++ build.py
@@ -10,46 +10,68 @@ GO_VERSIONS = {
         "v0.9.0":  "1.20.2",
         "v0.9.1":  "1.20.2",
         "v0.9.2":  "1.20.2",
+        "v0.9.3-1":  "1.20.2",
         "sdk-50":  "1.21.3",
     },
     "feeder": {
         "v0.8.3": "1.20.2",
+        "v0.11.0": "1.20.2",
+    },
+    "relayer": {
+        "v2.5.0": "1.21.7",
+    }
+}
+
+VERSIONS = {
+    "v0.1.0": {
+        "kujira": "v0.9.3-1",
+        "feeder": "v0.11.0",
+        "relayer": "v2.5.0"
     }
 }
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["build", "manifest"])
     parser.add_argument("-n", "--namespace", default="teamkujira")
-    parser.add_argument("-a", "--app", required=True)
-    parser.add_argument("-t", "--tag", required=True)
+    parser.add_argument("-a", "--app")
+    parser.add_argument("-t", "--tag", default="v0.1.0")
     parser.add_argument("--push", action="store_true")
     parser.add_argument("--podman", action="store_true")
-    parser.add_argument("--mainnet", action="store_true")
-    parser.add_argument("--testnet", action="store_true")
+    parser.add_argument("--manifest", action="store_true")
+    parser.add_argument("--archs", default=platform.machine())
     return parser.parse_args()
 
 
-def build(command, namespace, app, tag, push=False, extra_tags=[]):
+def build_arg(s):
+    return ["--build-arg", s]
+
+
+def build(command, namespace, app, tag, push=False):
     arch = platform.machine()
     fulltag = f"{namespace}/{app}:{tag}-{arch}"
-    baseapp = app.replace("prepare-", "")
 
     cmd = [command, "build", "--tag", fulltag]
 
-    if baseapp in ["feeder", "kujira"]:
-        go_version = GO_VERSIONS.get(baseapp, {}).get(tag)
+    if app in ["feeder", "kujira", "relayer"]:
+        go_version = GO_VERSIONS.get(app, {}).get(tag)
         if not go_version:
-            print(f"no go version defined for {baseapp}:{tag}")
+            print(f"no go version defined for {app}:{tag}")
             sys.exit(1)
 
-        cmd += ["--build-arg", f"{baseapp}_version={tag}"]
-        cmd += ["--build-arg", f"go_version={go_version}"]
+        cmd += build_arg(f"{app}_version={tag}")
+        cmd += build_arg(f"go_version={go_version}")
 
-    if app.startswith("prepare-"):
-        cmd += ["--build-arg", f"namespace={namespace}"]
-        cmd += ["--build-arg", f"arch={arch}"]
+    if app == "prepare":
+        kujira_version = VERSIONS[tag]["kujira"]
+        feeder_version = VERSIONS[tag]["feeder"]
+        relayer_version = VERSIONS[tag]["relayer"]
+
+        cmd += build_arg(f"kujira_version={kujira_version}")
+        cmd += build_arg(f"feeder_version={feeder_version}")
+        cmd += build_arg(f"relayer_version={relayer_version}")
+        cmd += build_arg(f"namespace={namespace}")
+        cmd += build_arg(f"arch={arch}")
 
     cmd.append(app)
 
@@ -60,12 +82,6 @@ def build(command, namespace, app, tag, push=False, extra_tags=[]):
         [command, "push", fulltag]
     ]
 
-    for tag in extra_tags:
-        print(tag)
-        newtag = f"{namespace}/{app}:{tag}-{arch}"
-        subprocess.run([command, "tag", fulltag, newtag])
-        push_cmds.append([command, "push", newtag])
-
     print(push_cmds)
 
     if push:
@@ -74,32 +90,22 @@ def build(command, namespace, app, tag, push=False, extra_tags=[]):
             subprocess.run(cmd)
 
 
-def manifest(command, namespace, app, tag, push=False, extra_tags=[]):
-    push_commands = []
-    basetag = f"{namespace}/{app}"
+def manifest(command, namespace, app, tag, archs, push=False):
+    fulltag = f"{namespace}/{app}:{tag}"
+    commands = []
 
-    fulltag = f"{basetag}:{tag}"
     print("------------------")
     print(fulltag)
-    subprocess.run([
-        command, "manifest", "create", f"{fulltag}",
-        f"{fulltag}-arm64", f"{fulltag}-x86_64"
-    ])
 
-    push_commands.append(
-        [command, "manifest", "push", f"{fulltag}"]
-    )
-
-    for tag in extra_tags:
-        print(tag)
-        newtag = f"{basetag}:{tag}"
-        subprocess.run([command, "tag", fulltag, newtag])
-        push_commands.append([command, "push", newtag])
+    tags = [f"{fulltag}-{x}" for x in archs.split(",")]
+    commands.append([command, "manifest", "create", f"{fulltag}"] + tags)
 
     if push:
-        for cmd in push_commands:
-            print(cmd)
-            subprocess.run(cmd)
+        commands.append([command, "manifest", "push", f"{fulltag}"])
+
+    for cmd in commands:
+        print(cmd)
+        subprocess.run(cmd)
 
 
 def main():
@@ -109,26 +115,25 @@ def main():
     if args.podman:
         command = "podman"
 
-    apps = [args.app]
-    if args.app in ["feeder", "kujira"]:
-        apps.append(f"prepare-{args.app}")
-
-    print(apps)
+    apps = ["kujira", "feeder", "relayer", "prepare"]
+    if args.app:
+        apps = [args.app]
 
     for app in apps:
-        extra_tags = []
-        if args.testnet:
-            extra_tags.append("testnet")
-        if args.mainnet:
-            extra_tags.append("mainnet")
+        tag = VERSIONS[args.tag].get(app)
+        if app == "prepare":
+            tag = args.tag
 
-        if args.action == "build":
+        if not tag:
+            print(f"no tag defined for {app} ({args.tag})")
+            sys.exit(1)
 
-            build(command, args.namespace, app,
-                  args.tag, args.push, extra_tags)
-        elif args.action == "manifest":
-            manifest(command, args.namespace, app,
-                     args.tag, args.push, extra_tags)
+        build(command, args.namespace, app, tag, args.push)
+
+        if not args.manifest:
+            return
+
+        manifest(command, args.namespace, app, tag, args.archs, args.push)
 
 
 if __name__ == "__main__":
---
+++ prepare/prepare.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python3
+
+import json
+import shutil
+import subprocess
+import os
+import jinja2
+import argparse
+
+WORKDIR = "pond"
+
+
+class Node:
+    denom = ""
+    chain_id = ""
+    port_prefix = 0
+    home = ""
+    binary = ""
+
+    def __init__(self, name, chain_id, binary, denom, port_prefix):
+        self.name = name
+        self.home = f"{WORKDIR}/{name}"
+        self.chain_id = chain_id
+        self.binary = binary
+        self.port_prefix = port_prefix
+        self.denom = denom
+
+    def init(self):
+        result = subprocess.run([
+            self.binary, "--home", self.home, "init", self.name,
+            "--chain-id", self.chain_id, "--default-denom", self.denom
+        ], capture_output=True, text=True)
+
+        output = json.loads(result.stderr)
+
+        return output["node_id"]
+
+    def add_key(self, mnemonic, dryrun=False):
+        extra = []
+        if dryrun:
+            extra = ["--dry-run"]
+
+        result = subprocess.run([
+            self.binary, "--output", "json",
+            "--home", self.home, "keys", "add", "validator",
+            "--keyring-backend", "test", "--recover"
+        ] + extra, capture_output=True, text=True, input=mnemonic)
+
+        output = json.loads(result.stdout)
+        address = output["address"]
+
+        return address
+
+    def add_genesis_account(self, address, amount):
+        subprocess.run([
+            self.binary, "--output", "json", "--home", self.home, "genesis",
+            "add-genesis-account", address, f"{amount}{self.denom}"
+        ])
+
+    def create_gentx(self, amount):
+        subprocess.run([
+            self.binary, "--output", "json", "--home", self.home, "genesis",
+            "gentx", "validator", f"{amount}{self.denom}",
+            "--chain-id", self.chain_id, "--keyring-backend", "test"
+        ], capture_output=True, text=True)
+
+        gentx_path = f"{self.home}/config/gentx"
+        gentx_file = os.listdir(gentx_path)[0]
+        gentx_data = json.load(open(f"{gentx_path}/{gentx_file}", "r"))
+        valoper = gentx_data["body"]["messages"][0]["validator_address"]
+
+        return valoper
+
+    def collectd_gentxs(self):
+        subprocess.run([
+            self.binary, "--home", self.home, "genesis", "collect-gentxs"
+        ], capture_output=False, text=True)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--nodes", type=int, default=1,
+                        choices=range(1, 9), metavar="[1-9]",
+                        help="Set number of validator nodes")
+    parser.add_argument("--clear", action="store_true",
+                        help="Remove all configs and create new chain")
+    parser.add_argument("--podman", action="store_true",
+                        help="Generate start/stop scripts for podman")
+
+    return parser.parse_args()
+
+
+def get_persistent_peers(validators, node_id, podman):
+    persistent_peers = []
+    for validator in validators:
+        if validator["node_id"] == node_id:
+            continue
+
+        port = validator["app_port"]
+        host = validator["moniker"].lower()
+        peer = validator["node_id"]
+        if podman:
+            host = "127.0.0.1"
+        persistent_peers.append(f"{peer}@{host}:{port}")
+
+    return persistent_peers
+
+
+def init_chain(name, chain_id, binary, denom, nodes, port_prefix, mnemonics, podman):
+    info = {
+        "validators": [],
+        "accounts": []
+    }
+
+    total = 0
+
+    main = Node(f"{name}1", id, binary, denom, port_prefix)
+
+    for i in range(nodes):
+        moniker = f"{name}{i+1}"
+        mnemonic = mnemonics["validators"][i]
+
+        amount = 300_000_000_000
+        staked = 200_000_000_000
+
+        total += amount
+
+        node = Node(moniker, chain_id, binary, denom, port_prefix)
+        node_id = node.init()
+        address = node.add_key(mnemonic)
+        node.add_genesis_account(address, amount)
+        valoper = node.create_gentx(staked)
+
+        # add account to "main" node
+        if i > 0:
+            main.add_genesis_account(address, amount)
+
+        info["validators"].append({
+            "moniker": moniker,
+            "address": address,
+            "valoper": valoper,
+            "node_id": node_id,
+            "mnemonic": mnemonic,
+            "api_port": f"{port_prefix}{i+1:02}17",
+            "app_port": f"{port_prefix}{i+1:02}56",
+            "rpc_port": f"{port_prefix}{i+1:02}57"
+        })
+
+        if i == 0:
+            continue
+
+        gentx_path = f"{WORKDIR}/{moniker}/config/gentx"
+        gentx_file = os.listdir(gentx_path)[0]
+
+        shutil.copyfile(
+            f"{gentx_path}/{gentx_file}",
+            f"{WORKDIR}/{name}1/config/gentx/{gentx_file}"
+        )
+
+    for i in range(0, len(mnemonics.get("accounts", []))):
+        mnemonic = mnemonics["accounts"][i]
+
+        amount = 10_000_000_000_000
+        total += amount
+
+        address = node.add_key(mnemonic, True)
+        node.add_genesis_account(address, amount)
+
+        info["accounts"].append({
+            "address": address,
+            "mnemonic": mnemonic
+        })
+
+    # add ibc account
+
+    mnemonic = mnemonics["relayer"]
+
+    amount = 1_000_000_000_000
+    total += amount
+
+    address = node.add_key(mnemonic, True)
+    node.add_genesis_account(address, amount)
+
+    info["ibc"] = {
+        "address": address,
+        "mnemonic": mnemonic
+    }
+
+    node.collectd_gentxs()
+
+    genesis = json.load(open(f"{node.home}/config/genesis.json"))
+
+    genesis["app_state"]["crisis"]["constant_fee"]["denom"] = denom
+    genesis["app_state"]["denom"]["params"]["creation_fee"][0]["denom"] = denom
+    genesis["app_state"]["gov"]["params"]["min_deposit"][0]["denom"] = denom
+    genesis["app_state"]["gov"]["params"]["max_deposit_period"] = "20s"
+    genesis["app_state"]["gov"]["params"]["voting_period"] = "20s"
+    genesis["app_state"]["mint"]["minter"]["inflation"] = "0"
+    genesis["app_state"]["mint"]["params"]["mint_denom"] = denom
+    genesis["app_state"]["staking"]["params"]["unbonding_time"] = "1209600s"
+    genesis["app_state"]["staking"]["params"]["bond_denom"] = denom
+
+    if name == "kujira-1":
+        genesis["app_state"]["oracle"]["params"]["whitelist"] = [
+            {"name": "BTC"},
+            {"name": "ETH"}
+        ]
+
+    for i in range(nodes):
+        filename = f"{WORKDIR}/{name}{i+1}/config/genesis.json"
+        json.dump(genesis, open(filename, "w"))
+
+    app_toml = jinja2.Template(
+        open("templates/app.toml.j2", "r").read()
+    )
+    config_toml = jinja2.Template(
+        open("templates/config.toml.j2", "r").read()
+    )
+    client_toml = jinja2.Template(
+        open("templates/client.toml.j2", "r").read()
+    )
+    feeder_toml = jinja2.Template(
+        open("templates/feeder.toml.j2", "r").read()
+    )
+
+    for i, validator in enumerate(info["validators"]):
+        # validator nodes
+
+        home = f"{WORKDIR}/{name}{i+1}"
+
+        node_id = validator["node_id"]
+
+        persistent_peers = get_persistent_peers(
+            info["validators"], node_id, podman
+        )
+
+        config = {
+            "api_port": f"{port_prefix}0{i+1}{17}",
+            "grpc_port": f"{port_prefix}0{i+1}{90}",
+            "app_port": f"{port_prefix}0{i+1}{56}",
+            "rpc_port": f"{port_prefix}0{i+1}{57}",
+            "abci_port": f"{port_prefix}0{i+1}{58}",
+            "pprof_port": f"{port_prefix}0{i+1}{60}",
+            "feeder_port": f"{port_prefix}0{i+1}{71}",
+            "persistent_peers": ",".join(persistent_peers),
+            "moniker": validator["moniker"],
+            "address": validator["address"],
+            "valoper": validator["valoper"],
+            "chain_id": chain_id,
+            "podman": podman
+        }
+
+        open(f"{home}/config/app.toml", "w").write(
+            app_toml.render(config)
+        )
+
+        open(f"{home}/config/config.toml", "w").write(
+            config_toml.render(config)
+        )
+
+        open(f"{home}/config/client.toml", "w").write(
+            client_toml.render(config)
+        )
+
+        # feeders
+        if chain_id != "kujira-1":
+            continue
+
+        home = f"{WORKDIR}/feeder1-{i+1}"
+
+        if not os.path.isdir(home):
+            os.mkdir(home)
+
+        open(f"{home}/config.toml", "w").write(
+            feeder_toml.render(config)
+        )
+
+    # fix info
+
+    for i in range(nodes):
+        validator = info["validators"][i]
+
+        validator["rpc_url"] = f"http://localhost:{validator['rpc_port']}"
+        validator["api_url"] = f"http://localhost:{validator['api_port']}"
+        validator.pop("app_port")
+        validator.pop("api_port")
+        validator.pop("rpc_port")
+
+        info["validators"][i] = validator
+
+    return info
+
+
+def main():
+    args = parse_args()
+
+    if args.clear:
+        shutil.rmtree(WORKDIR, ignore_errors=True)
+
+    if not os.path.isdir(WORKDIR):
+        os.mkdir(WORKDIR)
+
+    config = {
+        "command": "docker",
+        "wallets": {},
+        "validators": {},
+        "version": {
+            "kujira": os.environ.get("KUJIRA_VERSION"),
+            "feeder": os.environ.get("FEEDER_VERSION"),
+            "relayer": os.environ.get("RELAYER_VERSION")
+        }
+    }
+
+    if args.podman:
+        config["command"] = "podman"
+
+    mnemonics = json.load(open("mnemonics.json", "r"))
+
+    info1 = init_chain(
+        "kujira1-", "kujira-1", "kujirad", "ukuji",
+        args.nodes, 1, mnemonics, args.podman
+    )
+
+    info2 = init_chain(
+        "kujira2-", "kujira-2", "kujirad", "ukuji", 1, 2, mnemonics, args.podman
+    )
+
+    config["validators"] = {
+        "kujira-1": info1["validators"],
+        "kujira-2": info2["validators"]
+    }
+
+    config["wallets"] = info1["accounts"]
+    config["ibc"] = info1["ibc"]
+
+    # init relayer
+
+    home = f"{WORKDIR}/relayer"
+    os.makedirs(f"{home}/config", exist_ok=True)
+
+    relayer_yaml = jinja2.Template(
+        open("templates/relayer.yaml.j2", "r").read()
+    )
+
+    open(f"{home}/config/config.yaml", "w").write(
+        relayer_yaml.render({"podman": args.podman})
+    )
+
+    shutil.copytree("keys", f"{home}/keys", dirs_exist_ok=True)
+
+    json.dump(config, open(f"{WORKDIR}/pond.json", "w"))
+
+
+if __name__ == "__main__":
+    main()
